763e690e67685bdf4eb05c55483bb87713bb3f11
Yusuf Rençber <86446300+Aermoss@users.noreply.github.com>
2023-06-18 19:08:08 +0300

    Add files via upload

---
+++ stbiPy/stbipy/__init__.py
@@ -0,0 +1,9 @@
+__version__ = "0.0.1"
+__author__ = "Yusuf Rençber"
+
+import os
+
+if os.name != "nt":
+    raise OSError("This module does not support the OS you are using.")
+
+from .stbipy import *
